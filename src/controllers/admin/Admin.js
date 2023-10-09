8f3dcf7954938b9fc24a561fe386d0ed4db7ee49
sbcreations14378 <dsak@gmail.com>
2023-10-09 21:48:14 +0530

    Request component changed

--- src/controllers/admin/Admin.js
+++ src/controllers/admin/Admin.js
@@ -1,37 +1,29 @@
-import React from 'react'
-import '../../components/css/admin_css/Admin.css';
-import {MdLogout} from 'react-icons/md'
-
+import React from "react";
+import "../../components/css/admin_css/Admin.css";
+import { MdLogout } from "react-icons/md";
 
 const Admin = () => {
-  
   return (
-    <div className='admin-main'>
+    <div className="admin-main">
       <div>
-        <a
-        href='/admin'
-        >
-        <button>Logout<MdLogout/></button><br/>
+        <a href="/admin">
+          <button>
+            Logout
+            <MdLogout />
+          </button>
+          <br />
         </a>
-        All Requests
-        <div>
-          Update Requests
-        </div>
-        <div>
-          DMC Requests
-        </div>
-        <div>
-          News and Event Request
-        </div>
-        <div>
-          Department Circular Requests
+        <div className="all-requests">
+          <span>All Requests</span> 
+        <div className="updates-requests">Update Requests</div>
+        <div className="dmc-requests">DMC Requests</div>
+        <div className="news-requests">News and Event Request</div>
+        <div className="dept-requests">Department Circular Requests</div>
         </div>
       </div>
-    <div>
-      Admin_login
+      <div>Admin_login</div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
