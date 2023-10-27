f37c3beae83a884edf4592732dae1657b4ce17ee
sbcreations14378 <dsak@gmail.com>
2023-10-27 21:35:03 +0530

    api modified

--- src/Backend/routes/grievance_routes/GrievanceRoutes.js
+++ src/Backend/routes/grievance_routes/GrievanceRoutes.js
@@ -5,7 +5,7 @@ const grievance = require('../../apis/grievance_api/StudentGrievance')
 
 router.post('/sendmail',mailing.send)
 router.get('/recieve',mailing.receive)
-router.post('/send_grievance',grievance.send_grievance)
-router.get('/recieve_grievance',grievance.receive)
+router.post('/send-grievance',grievance.send_grievance)
+router.get('/recieve-grievance',grievance.receive)
 
 module.exports=router
