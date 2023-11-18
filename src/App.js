cdee5056d5882d2c1d4eb9bcd23c51de6e3b27d0
sbcreations14378 <dsak@gmail.com>
2023-11-18 11:45:38 +0530

    Routes case senstivity changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import DMC from "./Frontend/components/dmc/DMC";
 import DMCUpload from "./Frontend/components/dmc/Upload";
 import DMCTenders from "./Frontend/components/dmc/Tenders";
 import HODS from "./Frontend/components/hods/HODS";
-import AffliatedColleges from "./Frontend/components/affliatedColleges/Affliated_colleges";
+import AffiliatedColleges from "./Frontend/components/affliatedColleges/Affliated_colleges";
 import FacultyGrievance from "./Frontend/components/grievances/FacultyGrievance";
 import ForgotPassword from "./Frontend/Authentications/Forgot_password";
 import Results from "./Frontend/components/results/Results";
@@ -24,20 +24,20 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<AdminLogin />} />
-          <Route exact path="/Results" element={<Results/>} />
+          <Route exact path="/results" element={<Results/>} />
           <Route exact path="/admin" element={<Admin/>} />
           <Route exact path="/admin-control" element={<Admin />} />
           <Route exact path="/updates" element={<Updates />} />
           <Route exact path="/updates" element={<Updates />} />
-          <Route exact path="/DMC" element={<DMC />}>
-           <Route exact path="DMC-upload" element={<DMCUpload />} />
-           <Route exact path="DMC-tenders" element={<DMCTenders/>} />
+          <Route exact path="/dmc" element={<DMC />}>
+           <Route exact path="dmc-upload" element={<DMCUpload />} />
+           <Route exact path="dmc-tenders" element={<DMCTenders/>} />
           </Route>
-        <Route exact path="/HODS" element={<HODS />} />
+        <Route exact path="/hods" element={<HODS />} />
         <Route
           exact
-          path="/Affliated-College"
-          element={<AffliatedColleges />}
+          path="/affiliated-college"
+          element={<AffiliatedColleges />}
         />
         <Route exact path="/grievances" element={<FacultyGrievance />} />
         <Route exact path="/passwordreset" element={<ForgotPassword />} />
